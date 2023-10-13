fee6525502e8c1f66dc697fb9950c0cc101643ca
Thiago Nunes <thiago_nunes11@hotmail.com>
2023-10-13 17:33:39 -0300

    deploy com docker

---
+++ src/test/java/br/com/thiagonunes/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiagonunes.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
